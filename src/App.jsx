fb036c4753c3134c3043f3d63e4ec888ed9957d9
kdpark <kdpark.phd@gmail.com>
2024-01-19 00:34:09 +0900

    1. 디버그용 video preview 끄기 - params.msj 2. 시선, 화면 영역 기반 집중도 변수 추가 - setEye 변수가 제대로 업데이트 되지 않음.

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,48 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import webgazer from "./WebGazer/src/index.mjs";
 
 function App() {
+  const [screen, setScreen] = useState([window.innerWidth, window.innerHeight]);
+  const [eye, setEye] = useState(false);
   useEffect(() => {
     async function startGaze() {
       // webgazer.params.storingPoints = false;
       await webgazer
         .setRegression("ridge")
         .setGazeListener((data, clock) => {
-          if (data) console.log(data);
+          if (data) {
+            if (0 < data.x && data.x < screen[0] && 0 < data.y && data.y < screen[1] ){ // 화면 내 영역 볼 때
+                setEye(true);
+                console.log(true); 
+                //console.log(eye, data.x, screen[0], data.y, screen[1]);
+            } else { // 화면 밖을 바라보았을 때
+              setEye(false);
+              console.log(false);
+            }
+            
+          } else { // 화면 밖으로 얼굴 나갔을 때
+            setEye(false);
+            console.log(false);
+          }
+          
         })
         .saveDataAcrossSessions(true)
+        .addMouseEventListeners()
         .begin();
+      webgazer.clearData();
+      webgazer.addMouseEventListeners();
       webgazer
         /* shows all video previews */
-        .showVideoPreview(false)
+        // .showVideo(true)
+        // .showVideoPreview(true)
+        // .showFaceOverlay(true)
+        // .showFaceFeedbackBox(true)
         /* shows a square every 100 milliseconds where current prediction is */
         .showPredictionPoints(true)
         /* Kalman Filter defaults to on. Can be toggled by user. */
         .applyKalmanFilter(true);
+      
 
       //Set up the webgazer video feedback.
       var setup = function () {
@@ -43,6 +66,15 @@ function App() {
       });
     };
   }, []);
+  
+
+  useEffect(() => {
+    const resizeListener = () => {
+      setScreen([window.innerWidth, window.innerHeight]);
+    };
+    window.addEventListener("resize", resizeListener);
+    
+  });
 
   return <div></div>;
 }
